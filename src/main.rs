08ed16114ddcd4c14993c17796ae255daeae9961
Igor Couto <igor.fcouto@gmail.com>
2021-09-23 23:21:46 -0300

    Max function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::vec;
 
 fn main() {
-    let pattern = String::from("walk").as_bytes();
+    let pattern: &[u8] = &['w' as u8, 'a' as u8, 'l' as u8, 'k' as u8];
     let text = String::from("Will you walk into my parlour? said a spider to a fly").as_bytes();
+}
+
+fn max(n1: u8, n2: u8) -> u8 {
+    if n1 > n2 {
+        n1
+    } else {
+        n2
+    }
 }
