5d6e7a9ad0ed7186889ac5231f3ee20e4c3c8090
Igor Couto <igor.fcouto@gmail.com>
2021-09-24 00:48:41 -0300

    WIP search

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ use ahash::AHashMap;
 
 fn main() {
     let pattern: &[u8] = &['w' as u8, 'a' as u8, 'l' as u8, 'k' as u8];
-    let text = String::from("Will you walk into my parlour? said a spider to a fly").as_bytes();
+    let text = String::from("Will you walk into my parlour? said a spider to a fly");
+    let text_bytes = text.as_bytes();
 
     let bad_match_table = pre_process_pattern(&pattern);
+
+    let result = search(&bad_match_table, &text_bytes);
 }
 
 fn pre_process_pattern(pattern: &[u8]) -> AHashMap<u8, u8> {
@@ -20,6 +23,23 @@ fn pre_process_pattern(pattern: &[u8]) -> AHashMap<u8, u8> {
     bad_match_table.insert('*' as u8, pattern.len() as u8);
 
     return bad_match_table;
+}
+
+fn search(bad_match_table: &AHashMap<u8, u8>, text: &[u8]) -> u32 {
+    let mut index = (bad_match_table.get(&('*' as u8)).unwrap() - 1u8) as usize;
+    loop {
+        if index >= text.len() {
+            break;
+        }
+
+        //  se o caracter do texto for igual ao ultimo do pattern começa uma busca para esquerda no pattern
+        //  até dar match compelto
+        //  em caso de mismatch
+        // consultar tabela e andar para a direita o numero de vezes indicado por value
+
+        index += 1;
+    }
+    return index as u32;
 }
 
 fn max(n1: u8, n2: u8) -> u8 {
